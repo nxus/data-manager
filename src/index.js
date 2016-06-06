f07658b8e9cd74910ffa4abfc845f3945165c9f3
Mike Reich <mjreich@gmail.com>
2016-06-06 08:53:18 -0700

    Added fix for multiple listeners on multiple uploads

--- src/index.js
+++ src/index.js
@@ -178,8 +178,10 @@ export default class DataLoader {
   // Internal
   
   _modelImport(model_id, results, opts) {
-    this.on('models.'+model_id, (args) => {return args})
-    this.on('model.'+model_id, (args) => {return args})
+    if(this.app.get('data-loader').listenerCount('models.'+model_id) == 0) {
+      this.on('models.'+model_id, (args) => {return args})
+      this.on('model.'+model_id, (args) => {return args})
+    }
     if (opts === undefined) opts = _defaultImportOptions
 
     if(typeof opts.strict != 'undefined' && opts.strict === false) {
